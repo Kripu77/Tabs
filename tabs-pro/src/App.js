298b76bbc2c4477230f53ed93de0e52705f3ce46
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-10-06 15:58:56 +1100

    Reset of all the files

--- tabs-pro/src/App.js
+++ tabs-pro/src/App.js
@@ -1,73 +1,13 @@
-import React, {useState, useEffect} from 'react'
-import Buttons from './components/Buttons';
-import Error from './components/Error';
-import Loading from './components/Loading';
-import Main from './components/Main';
-const url = "https://course-api.com/react-tabs-project";
-
-//to fetch the button title as per the company
-
+import React, {useState, useEffect} from 'react';
+const URL = "https://course-api.com/react-tabs-project";
 
 const App = () => {
-    const[section, setSection] = useState([]);
-    const[isLoading, setIsLoading] = useState(true);
-    const[isError, setIsError]=  useState(false)
-    const filterBtn = (value)=>{
-        if(value==="TOMMY"){
-            setSection(section)
-        }
-setSection(section.filter((values)=>{
-    return values.company === value;
-
-}))
-}
-
-    //lets fetch data and useEffect hook
-    const fetchData = ()=>{
-        fetch(url) 
-        .then((res)=>{ 
-            if(res.status>200|| res.status<299){
-              
-                  return res.json();
-                  
-                  
-            }
-            else{
-                 setIsError(true)
-                 setIsLoading(false);     
-                  }
-          })
-        .then((data)=>{
-            setIsLoading(false)
-            setSection(data);
-        })
-    }
-  
-
-    useEffect(()=>{
-      fetchData();    },[])
 
 
-     
-const allBtns = new Set(section.map((value)=>{
-    return value.company;
-}))
-
-const fnlBtn =  Array.from(allBtns);
-
-
-
-    if(isLoading){
-        return <Loading/> 
-    }
-    if(isError){
-        return <Error/>
-    }
+    //fetch data 1st from the api
     return (
         <div>
-            <h1> Experience</h1>
-            <Buttons fnlBtn={fnlBtn} filterBtn={filterBtn}/>
-          <Main section={section}/>
+            <h1> test</h1>
         </div>
     )
 }
